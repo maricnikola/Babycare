effc509f71b1603f67c31d2ccc23a5721bc0cdb7
Nikola <n.maric1912@gmail.com>
2025-09-19 02:29:05 +0200

    feat: add addExamination endpoint

---
+++ Babycare/service/src/main/java/com/ftn/sbnz/service/ExaminationService.java
@@ -0,0 +1,39 @@
+package com.ftn.sbnz.service;
+
+import com.ftn.sbnz.ServiceApplication;
+import com.ftn.sbnz.model.dtos.ExaminationDTO;
+import com.ftn.sbnz.model.models.Baby;
+import com.ftn.sbnz.model.models.Examination;
+import com.ftn.sbnz.model.util.KnowledgeSessionHelper;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExaminationService {
+
+    @Autowired
+    private KieContainer kieContainer;
+
+    public void addExamination(Baby baby, ExaminationDTO examinationDTO) {
+        Examination examination = new Examination();
+        examination.setExamDate(examinationDTO.getExamDate());
+        examination.setHeight(examinationDTO.getHeight());
+        examination.setWeight(examinationDTO.getWeight());
+        examination.setTemperature(examinationDTO.getTemperature());
+        examination.setHeartRate(examinationDTO.getHeartRate());
+        examination.setRespirationRate(examinationDTO.getRespirationRate());
+
+        examination.setBaby(baby);
+
+        baby.getExaminations().add(examination);
+
+        KieSession kieSession = KnowledgeSessionHelper.getStatefulKnowledgeSession(kieContainer, "test-session");
+
+        kieSession.insert(baby);
+        kieSession.insert(examination);
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
+}
