b0bd9d8ddf22da323973dec41472d7dd9adf4181
Nikola <n.maric1912@gmail.com>
2025-10-04 18:48:03 +0200

    feat: add pages for baby create and examination create

--- Babycare-ui/src/app/app.config.ts
+++ Babycare-ui/src/app/app.config.ts
@@ -1,12 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura,
+            options: {
+                prefix: 'p',
+                darkModeSelector: 'off',
+                cssLayer: false
+            }
+        }
+    })
   ]
 };
