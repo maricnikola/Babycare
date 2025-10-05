b6c581ce8b71a2c334ca27125b66434179f3f056
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2025-10-05 22:45:04 +0200

    feat: add handling for AlarmEvent notifications in MonitoringService via WebSocket

--- Babycare/service/src/main/java/com/ftn/sbnz/service/MonitoringService.java
+++ Babycare/service/src/main/java/com/ftn/sbnz/service/MonitoringService.java
@@ -64,7 +64,7 @@ public class MonitoringService {
         kieSession.addEventListener(new DefaultRuleRuntimeEventListener() {
             @Override
             public void objectInserted(ObjectInsertedEvent event) {
-                Object fact = event.getObject();   
+                Object fact = event.getObject();
                 if (fact instanceof AlarmEvent) {
                     String msg = ((AlarmEvent) fact).getMessage();
                     webSocketService.sendToTopic("/topic/alarm", msg);
